e58ab1b0fe6f4ac781643e68d54e66009de14f59
diivanych <diivanych@list.ru>
2023-02-12 00:20:43 +0300

    Найти max из двух чисел

--- HomeWork/Task01/Program.cs
+++ HomeWork/Task01/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if(num1 > num2)
+{
+    Console.WriteLine($"Максимальным числом будет {num1}");
+}
+else
+{
+    Console.WriteLine($"Максимальным числом будет {num2}");
+}
