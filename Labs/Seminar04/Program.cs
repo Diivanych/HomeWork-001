c214a1f83f25b1f7024ad1f5ea7bfd0f2b74e5e1
diivanych <diivanych@list.ru>
2023-02-11 22:45:43 +0300

    Целые от - до +

---
+++ Labs/Seminar04/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int curent = num * (-1);
+while (curent <= num)
+{
+    Console.Write($" {curent} " );
+    curent++;
+}
+
+// Console.WriteLine($"Квадрат числа {num} есть {quad}");
