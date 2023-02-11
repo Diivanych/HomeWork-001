a45bd4415c34d9a7dea227bdde8032f1a5c5cc4f
diivanych <diivanych@list.ru>
2023-02-12 01:16:09 +0300

    Чётные в заданном диапазон

---
+++ HomeWork/Task04/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int start = 1;
+Console.WriteLine($"Чётными числами в диапазоне от 1 до {num} будет");
+while (start <= num)
+{
+    if(start%2 == 0)
+    {
+        Console.Write($" {start} ");
+    }
+    start++;
+}
