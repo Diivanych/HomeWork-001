22b25ec8ef86224b3036fb93065cf7e34810afc6
diivanych <diivanych@list.ru>
2023-02-11 23:32:42 +0300

    Остаток от деления на 10

--- Labs/Seminar05/Program.cs
+++ Labs/Seminar05/Program.cs
@@ -1,6 +1,6 @@
 ﻿Console.WriteLine("Введите число");
 int num = Convert.ToInt32(Console.ReadLine());
-if(num > 99 && num < 1000)
+if(num > 99 && num < 1000) // Применили логическое "И" &&. Есть ещё логическое "ИЛИ" || 
 {
     Console.WriteLine(num%10);
 }
