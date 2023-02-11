905be2f73169f6af493c49b6f06aa3cdd6d72c77
diivanych <diivanych@list.ru>
2023-02-11 23:27:46 +0300

    Остаток от деления на 10

---
+++ Labs/Seminar05/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num > 99 && num < 1000)
+{
+    Console.WriteLine(num%10);
+}
+else
+{
+    Console.WriteLine("Число должно быть трёхзначным");
+}
