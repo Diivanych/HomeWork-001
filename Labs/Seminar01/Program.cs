1770eeffef2177d7457ac60d29af47dd2cdbe859
diivanych <diivanych@list.ru>
2023-02-11 20:24:26 +0300

    Начинаем домашнее задание

---
+++ Labs/Seminar01/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("GB Lesson01");
+int num = 5;
+int num2 = Convert.ToInt32(Console.ReadLine());  //  ToInt32 - преобразует символ в целое число
+Console.WriteLine(num + " и " + num2);  //  Вывод двух переменных и текста в одном операторе
