0b6aa201a2cb4a2aba00322b2c1f3683c4cbd283
diivanych <diivanych@list.ru>
2023-02-12 02:17:26 +0300

    последняя цифра в трхзначном числе

--- Labs/Seminar05/Program.cs
+++ Labs/Seminar05/Program.cs
@@ -1,5 +1,9 @@
 ﻿Console.WriteLine("Введите трёхзначное число");
 int num = Convert.ToInt32(Console.ReadLine());
+if(num < 0)
+{
+    num = num * (-1);
+}
 if(num > 99 && num < 1000) // Применили логическое "И" &&. Есть ещё логическое "ИЛИ" || 
 {
     Console.WriteLine($"Последняя цифра в этом трёхзначном числе будет {num%10}");
