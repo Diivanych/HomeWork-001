c432a6521c32496ca7153e547d855fcb21df9b72
diivanych <diivanych@list.ru>
2023-02-11 23:34:45 +0300

    Остаток от деления на 10

--- Labs/Seminar05/Program.cs
+++ Labs/Seminar05/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.WriteLine("Введите число");
+﻿Console.WriteLine("Введите трёхзначное число");
 int num = Convert.ToInt32(Console.ReadLine());
 if(num > 99 && num < 1000) // Применили логическое "И" &&. Есть ещё логическое "ИЛИ" || 
 {
