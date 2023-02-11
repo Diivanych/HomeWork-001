d389b1e7c709ef59bf82b71892956a5137fb4178
diivanych <diivanych@list.ru>
2023-02-11 21:08:03 +0300

    Квадрат числа-2

--- Labs/Seminar02/Program.cs
+++ Labs/Seminar02/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());  //  ToInt32 - преобразует символ в целое число
+int quad = num * num;
+Console.WriteLine($"Квадрат числа {num} есть {quad}");  //  Вывод текста и переменных в одной строке
