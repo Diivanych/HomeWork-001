5c5eb9f302744d57668edc54bc5ef788fce12622
diivanych <diivanych@list.ru>
2023-02-12 00:31:03 +0300

    Найти max из трёх чисел

--- HomeWork/Task02/Program.cs
+++ HomeWork/Task02/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int num3 = Convert.ToInt32(Console.ReadLine());
+if(num1 > num2)
+{
+    Console.WriteLine($"Максимальным числом будет {num1}");
+}
+else
+{
+    if(num2 > num3)
+    {
+        Console.WriteLine($"Максимальным числом будет {num2}");
+    }
+    else
+    {
+        Console.WriteLine($"Максимальным числом будет {num3}");
+    }
+}
